bf548725cd18fd3102f0dcef6b3f8b046492e4c0
Honorio <htaveras90@gmail.com>
2020-12-31 17:58:20 -0700

    adding a components screen component to app

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const ComponentsScreen = () => {
+  return <Text>Components Screen</Text>;
+};
+
+export default ComponentsScreen;
